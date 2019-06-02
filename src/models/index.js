0849318433772061704eb8b53a908dbe782d8838
tomastrg <tomastrg22@gmail.com>
2019-06-02 12:31:10 +0200

    add npm script format

--- src/models/index.js
+++ src/models/index.js
@@ -5,17 +5,15 @@ import Message from './message';
 
 const connectDb = () => {
   if (process.env.TEST_DATABASE_URL) {
-    return mongoose.connect(
-      process.env.TEST_DATABASE_URL,
-      { useNewUrlParser: true },
-    );
+    return mongoose.connect(process.env.TEST_DATABASE_URL, {
+      useNewUrlParser: true,
+    });
   }
 
   if (process.env.DATABASE_URL) {
-    return mongoose.connect(
-      process.env.DATABASE_URL,
-      { useNewUrlParser: true },
-    );
+    return mongoose.connect(process.env.DATABASE_URL, {
+      useNewUrlParser: true,
+    });
   }
 };
 
